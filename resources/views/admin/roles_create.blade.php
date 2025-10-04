34a67911e768b918f43bddef40a7630899a750c8
mostafaramdan5544@gmail.com <mostafaramdan5544@gmail.com>
2025-10-04 08:30:32 +0300

    feat: add localization for patient and visit permissions in Arabic and English

--- resources/views/admin/roles_create.blade.php
+++ resources/views/admin/roles_create.blade.php
@@ -17,7 +17,7 @@
             <div class="col-md-4 mb-2">
               <div class="form-check">
                 <input class="form-check-input" type="checkbox" name="permissions[]" value="{{ $perm->name }}" id="perm_{{ $perm->id }}">
-                <label class="form-check-label" for="perm_{{ $perm->id }}">{{ $perm->name }}</label>
+                <label class="form-check-label" for="perm_{{ $perm->id }}">{{ __('messages.permissions.' . $perm->name) }}</label>
               </div>
             </div>
           @endforeach
